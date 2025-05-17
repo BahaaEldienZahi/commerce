498eb6462e2b0ea10be3e8f7dff772d77c0f4368
BahaaEldinZahi <hamadazahi699@gmail.com>
2025-05-18 01:22:01 +0300

    Add product display and update logout functionality

--- js/script.js
+++ js/script.js
@@ -15,4 +15,47 @@ if (username) {
     links.remove()
     userinfo.style.display = "flex";
     userdom.innerHTML = username;
-}
+};
+
+logoutbtn.addEventListener("click", function (e) {
+    e.preventDefault();
+    localStorage.clear();
+    setTimeout(() => {
+        window.location = "register.html";
+    }, 1500)
+});
+
+// define products
+
+let products = [
+    {
+        id: 1,
+        name: "Product 1",
+        price: 100,
+        image: "img/product1.jpg",
+    },
+    {
+        id: 2,
+        name: "Product 2",
+        price: 200,
+        image: "img/product2.jpg",
+    },
+    {
+        id: 3,
+        name: "Product 3",
+        price: 300,
+        image: "img/product3.jpg",
+    },
+    {
+        id: 4,
+        name: "Product 4",
+        price: 400,
+        image: "img/product4.jpg",
+    },
+    {
+        id: 5,
+        name: "Product 5",
+        price: 500,
+        image: "img/product5.jpg",
+    },
+];
